365232331d6faa02b4ed367f5bc94cc97723c9de
Anggelinna <lina.koloytanova.03@yandex.ru>
2024-07-11 22:01:12 +0500

    .

--- vite-project/.eslintrc.cjs
+++ vite-project/.eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "react/prop-types": "off",
+      // Скрываем ошибку POPS
+      "react/prop-types":"off",
   },
 }
