f94a7179291ba069e6bfad42b239f1763c5fa9cf
Anggelinna <142165657+Anggelinna@users.noreply.github.com>
2025-03-10 22:32:15 +0500

    ispravleno

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,31 +8,29 @@ const Header = () => {
             </div>
 
 
-            
-			<div class="container">
-				<div class="header__block">
-					<div class="header__logo _show _light">
+
+			<div className="header__block">
+					<div className="header__logo _show _light">
 						<a href="" target="_self"><img src="images/logo.png" alt="logo"></img></a>
 					</div>
-					<div class="header__logo _dark">
+					<div className="header__logo _dark">
 						<a href="" target="_self"><img src="images/logo_dark.png" alt="logo"></img></a>
 					</div>
-					<nav class="header__nav">
+					<nav className="header__nav">
 						<button class="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
 						<a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
-						<div class="header__pop-user-set pop-user-set" id="user-set-target">
+						<div className="header__pop-user-set pop-user-set" id="user-set-target">
 							<a href="">x</a>
-							<p class="pop-user-set__name">Ivan Ivanov</p>
-							<p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-							<div class="pop-user-set__theme">
+							<p className="pop-user-set__name">Ivan Ivanov</p>
+							<p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+							<div className="pop-user-set__theme">
 								<p>Темная тема</p>
 								<input type="checkbox" class="checkbox" name="checkbox"></input>
 							</div>
-                            <button type="button" class="_hover03"><a href="#popExit">Выйти</a></button>
+                            <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
 						</div>
 					</nav>					
 				</div>
-			</div>
             			
         </header>
 
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+    return (
+
+		<main className="main">
+			<div className="container">
+				
+				<div className="main__block">
+					<div className="main__content"></div>
+				</div>
+			</div>
+	    </main>
+
+	)
+}
+export default Main;
+
