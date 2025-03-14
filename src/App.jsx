90f191245ee410e815bd27f7f6674cb6b68dab42
Anggelinna <142165657+Anggelinna@users.noreply.github.com>
2025-03-15 00:10:56 +0500

    исправл

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Header } from "./components/Header"
-import { Main } from "./components/Main";
+import { Header } from "./components/Header/Header"
+import { Main } from "./components/Main/Main";
 
 
 function App() {
