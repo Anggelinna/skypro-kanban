513df75de9febcfbd705c25a4aa2988f58b19145
Anggelinna <lina.koloytanova.03@yandex.ru>
2025-03-20 16:51:24 +0500

    isprav

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,55 +1,17 @@
 import { Card } from "../Card/Card";
 export const Column = ({ title }) => {
-    return (
-        <div className="main__column column">
-							<div className="column__title">
-								<p>{title}</p>
-							</div>
-							<div className="cards">
-								<Card title="Research" />
-								<Card title="Web Design" />
-								<Card title="Copywriting" />
-							
-							</div>
-						</div>		
-    );
-}
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+      <div className="cards">
+        <Card title="Research" />
+        <Card title="Web Design" />
+        <Card title="Copywriting" />
+      </div>
+    </div>
+  );
+};
 
 //export default Column;
-
-//const Column = () => {
-//    return (
-//
-//    <div className="main__column">
-//		<div className="column__title">
-//			<p>Нужно сделать</p>
-//		</div>
- //   
-
-//        <div className="main__column column">
-//	    	<div className="column__title">
-//	    		<p>Без статуса</p>
-//	    	</div>
-//	    </div>				
-						
-//	    <div className="main__column">
-//	    	<div className="column__title">
-//	    		<p>В работе</p>
-//	        </div>
-//	    </div>
-//
-//	    <div className="main__column">
-//	    	<div className="column__title">
-//	    	    <p>Тестирование</p>
-//	    	</div>
-//        </div>
-
-//        <div className="main__column">
-//	    	<div className="column__title">
-//	    		<p>Готово</p>
-//	    	</div>
-//        </div>
-//	</div>
-
-//	);
-//}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,35 +1,20 @@
 import { Column } from "../Column/Column";
 export const Main = () => {
- return (
-	<main className="main">
-		<div className="container">
-			<div className="main__block">
-				<div className="main__content">
-					<Column title="Без статуса" />
-					<Column title="Нужно сделать" />
-					<Column title="В работе" />
-					<Column title="Тестирование" />
-					<Column title="Готово" />
-				</div>
-			</div>
-		</div>
-	</main>
- );
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="main__block">
+          <div className="main__content">
+            <Column title="Без статуса" />
+            <Column title="Нужно сделать" />
+            <Column title="В работе" />
+            <Column title="Тестирование" />
+            <Column title="Готово" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 //export default Main;
-
-//const Main = () => {
-//    return (
-//
-//		<main className="main">
-//			<div className="container">
-//				
-//				<div className="main__block">
-//					<div className="main__content"></div>
-//				</div>
-//			</div>
-//	    </main>
-
-//	)
-//}
