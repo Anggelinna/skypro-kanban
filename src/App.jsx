77288f23ff4f3c190216e36f5bfe71310a9a74bf
Anggelinna <lina.koloytanova.03@yandex.ru>
2025-03-20 16:53:13 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -19,37 +19,3 @@ export default App;
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-
-//function App() {
-//  const [count, setCount] = useState(0)
-
-//  return (
-//    <>
-//      <div>
-//        <a href="https://vite.dev" target="_blank">
-//          <img src={viteLogo} className="logo" alt="Vite logo" />
-//        </a>
-//        <a href="https://react.dev" target="_blank">
-//          <img src={reactLogo} className="logo react" alt="React logo" />
-//        </a>
-//      </div>
-//      <h1>Vite + React</h1>
-//      <div className="card">
-//        <button onClick={() => setCount((count) => count + 1)}>
-//          count is {count}
-//        </button>
-//        <p>
-//          <code>src/App.jsx</code>
-//        </p>
-//      </div>
-//      <p className="read-the-docs">
-//        Click on the Vite and React logos to learn more
-//      </p>
-
-//    <div class="wrapper"></div>
-//    <script src="js/script.js"></script>
-
-//	</>
-
-//  );
-//}
