7eaa2cad1f04b2677751cebbdea105f15284ed29
Anggelinna <lina.koloytanova.03@yandex.ru>
2025-04-29 00:31:20 +0500

    column

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -2,23 +2,22 @@ import { Card } from "../Card/Card";
 import * as S from "./Column.styled";
 
 export const Column = ({ title, tasks }) => {
-    return (
-        <S.MainColumn>
-			<S.ColumnTitle>
-				<p>{title}</p>
-			</S.ColumnTitle>
-			<S.Cards>
-				{tasks.map((card) => (
-					<Card 
-					    topic={card.topic}
-						//theme={card.theme} 
-						title={card.title}
-						date={card.date}
-						key={card._id}
-						id={card._id}
-					/>
-				))}
-			</S.Cards>
-		</S.MainColumn>		
-    )
+	return (
+		<S.MainColumn>
+		<S.ColumnTitle>
+		  <p>{title}</p>
+		</S.ColumnTitle>
+		<S.StyledCard>
+		  {tasks.map((card) =>
+			  <Card
+				topic={card.topic}
+				title={card.title}
+				date={card.date}
+				key={card._id}
+				id={card._id}
+			  />)
+		  }
+		</S.StyledCard>
+	  </S.MainColumn>
+	)
 }
--- src/components/Column/Column.styled.js
+++ src/components/Column/Column.styled.js
@@ -1,35 +1,42 @@
 import styled from "styled-components";
 
 export const MainColumn = styled.div`
-    width: 20%;
-    margin: 0 auto;
-    display: block;
-    @media screen and (max-width: 1200px) {
-        width: 100%;
-        margin: 0 auto;
-        display: block;
-    }
+  width: 20%;
+  margin: 0 auto;
+  display: block;
 `;
 
 export const ColumnTitle = styled.div`
-    padding: 0 10px;
-    margin: 15px 0;
-    p {
-        color: #94A6BE;
-        font-size: 14px;
-        font-weight: 600;
-        line-height: 1;
-        text-transform: uppercase;
-    }
+  padding: 0 10px;
+  margin: 15px 0;
+  align-items: center;
+  color: rgb(148, 166, 190);
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 16px;
+  letter-spacing: 0%;
+  text-align: left;
+  text-transform: uppercase;
+
+  p {
+    color: #94A6BE;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1;
+    text-transform: uppercase;
+    display: flex;
+  }
 `;
 
-export const Cards = styled.div`
+export const StyledCard = styled.div`
+  width: 100%;
+  display: block;
+  position: relative;
+
+  @media screen and (max-width: 1200px) {
     width: 100%;
-    display: block;
-    position: relative;
-    @media screen and (max-width: 1200px) {
-        width: 100%;
-        display: flex;
-        overflow-y: auto;   
-    }
+    display: flex;
+    overflow-y: auto;
+  }
 `;
